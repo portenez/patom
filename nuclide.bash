3a320e085dcfc41768aa2505fbbe872ba28e20bc
portenez <vh.garcia.valencia@gmail.com>
2016-09-14 21:26:59 -0400

    added nuclide

---
+++ nuclide.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+apm install tool-bar
+apm install sort-lines
+apm install language-ocaml
+apm install language-babel
+apm install nuclide
---
+++ nuclide.md
@@ -0,0 +1,7 @@
+# nuclide instructions
+
+1. Install watchman
+```bash
+brew update
+brew install watchman
+```
